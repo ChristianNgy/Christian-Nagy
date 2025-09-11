909a220652add14959aeacc2e9a196b023e0f74d
ChrisNgy <c.nagy@students.htl-leonding.ac.at>
2025-09-11 16:21:05 +0200

    Refactor counter functionality in HTML and TypeScript files

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -1,16 +1,23 @@
-const counterButton = document.getElementById('counterButton') as HTMLButtonElement;
+const increaseButton = document.getElementById('increaseButton') as HTMLButtonElement;
 const counter = document.getElementById('counter') as HTMLParagraphElement;
-const decreaseButton = document.getElementById('deleteButton') as HTMLButtonElement;
+const decreaseButton = document.getElementById('decreaseButton') as HTMLButtonElement;
 
 
 let clickCount = 0;
 
-counterButton.addEventListener('click', () => {
+increaseButton.addEventListener('click', () => {
   clickCount++;
   counter.textContent = `${clickCount}`;
+
+  if(clickCount >= 0){
+    counter.style.color = 'black'
+  }
 });
 
 decreaseButton.addEventListener('click', () => {
     clickCount--
   counter.textContent = `${clickCount}`;
+  if(clickCount < 0){
+    counter.style.color = 'red'
+  }
 })
