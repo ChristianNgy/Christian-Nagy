39c12bc43b7854de9ffedc9117d2cc4bea4010d1
ChrisNgy <c.nagy@students.htl-leonding.ac.at>
2025-09-11 15:59:23 +0200

    Replace header with counter functionality in HTML and TypeScript files

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -1,3 +1,9 @@
-const helloWorldHeader = document.getElementById("helloWorldHeader") as HTMLHeadingElement;
+const counterButton = document.getElementById('counterButton') as HTMLButtonElement;
+const counter = document.getElementById('counter') as HTMLParagraphElement;
 
-helloWorldHeader.innerText += "!";
+let clickCount = 0;
+
+counterButton.addEventListener('click', () => {
+  clickCount++;
+  counter.textContent = `${clickCount}`;
+});
