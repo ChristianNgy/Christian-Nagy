df10ccb853df58f728ba0d3ed57105f7aefd0fb3
ChrisNgy <c.nagy@students.htl-leonding.ac.at>
2025-09-27 22:00:40 +0200

    Enhance SVG shape generator: add rectangle and triangle creation logic

--- homework/20-svg/src/index.ts
+++ homework/20-svg/src/index.ts
@@ -1,5 +1,42 @@
-const select = document.getElementById('shapeForm') as HTMLSelectElement
-const button = document.getElementById('shapeButton') as HTMLButtonElement
-const shape = document.getElementById('svg') as unknown as SVGSVGElement
+const select = document.getElementById('shapeForm') as HTMLSelectElement;
+const button = document.getElementById('add') as HTMLButtonElement;
+const shape = document.getElementById('svg') as unknown as SVGSVGElement;
 
-let shapeForm = ''
+let shapeForm = '';
+
+select.addEventListener('change', () => {
+  shapeForm = select.value;
+});
+
+button.addEventListener('click', () => {
+  const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
+  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect') as unknown as SVGCircleElement;
+  const triangle = document.createElementNS('http://www.w3.org/2000/svg', 'triangle') as SVGCircleElement;
+  
+  if (shapeForm === 'circle') {
+    circle.setAttribute('fill', 'white');
+    circle.setAttribute('stroke', 'black');
+    circle.setAttribute('cx', `${Math.random() * 200}`);
+    circle.setAttribute('cy', `${Math.random() * 200}`);
+    circle.setAttribute('r', `${Math.random() * 50}`);
+    shape.appendChild(circle);
+  } else if (shapeForm === 'rect') {
+    rect.setAttribute('fill', 'white');
+    rect.setAttribute('stroke', 'black');
+    rect.setAttribute('x', `${Math.random() * 200}`);
+    rect.setAttribute('y', `${Math.random() * 200}`);
+    rect.setAttribute('width', `${Math.random() * 100}`);
+    rect.setAttribute('height', `${Math.random() * 100}`);
+    shape.appendChild(rect);
+  } else if (shapeForm === 'triangle') {
+    triangle.setAttribute('fill', 'lightblue');
+    triangle.setAttribute('stroke', 'green');
+    triangle.setAttribute('x1', `${Math.random() * 200}`);
+    triangle.setAttribute('y1', `${Math.random() * 200}`);
+    triangle.setAttribute('x2', `${Math.random() * 200}`);
+    triangle.setAttribute('y2', `${Math.random() * 200}`);
+    triangle.setAttribute('x3', `${Math.random() * 200}`);
+    triangle.setAttribute('y3', `${Math.random() * 200}`);
+    shape.appendChild(triangle);
+  }
+});
