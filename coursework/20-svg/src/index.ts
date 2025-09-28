dcf6d8fb4d95f2e2c9b95acf075072e6434c492f
ChrisNgy <c.nagy@students.htl-leonding.ac.at>
2025-09-28 16:41:41 +0200

    Refactor HTML and TypeScript: standardize formatting and improve readability

--- coursework/20-svg/src/index.ts
+++ coursework/20-svg/src/index.ts
@@ -1,15 +1,14 @@
-const svg = document.getElementById("svg") as unknown as SVGSVGElement;
-const add = document.getElementById("add") as HTMLButtonElement
+const svg = document.getElementById('svg') as unknown as SVGSVGElement;
+const add = document.getElementById('add') as HTMLButtonElement;
 
-add.addEventListener('click', () =>{
-    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
+add.addEventListener('click', () => {
+  const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
 
-    circle.setAttribute('fill', 'lime')
-    circle.setAttribute('stroke', 'green')
-    circle.setAttribute('cx', `${Math.random() * 200}`)
-    circle.setAttribute('cy', `${Math.random() * 200}`)
-    circle.setAttribute('r', `${Math.random() * 50}`)
+  circle.setAttribute('fill', 'lime');
+  circle.setAttribute('stroke', 'green');
+  circle.setAttribute('cx', `${Math.random() * 200}`);
+  circle.setAttribute('cy', `${Math.random() * 200}`);
+  circle.setAttribute('r', `${Math.random() * 50}`);
 
-    svg.appendChild(circle)
-
-})
+  svg.appendChild(circle);
+});
