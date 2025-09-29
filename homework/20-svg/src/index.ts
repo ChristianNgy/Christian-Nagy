2c01efacf0ecde3651f99aef4115e0c42079bf95
ChrisNgy <c.nagy@students.htl-leonding.ac.at>
2025-09-29 12:30:27 +0200

    Enhance shape creation: use random colors for circle, rectangle, and triangle fills and strokes

--- homework/20-svg/src/index.ts
+++ homework/20-svg/src/index.ts
@@ -12,25 +12,28 @@ button.addEventListener('click', () => {
   const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
   const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect') as unknown as SVGCircleElement;
   const triangle = document.createElementNS('http://www.w3.org/2000/svg', 'triangle') as SVGCircleElement;
+  function randomColor(): string {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+}
   
   if (shapeForm === 'circle') {
-    circle.setAttribute('fill', 'white');
-    circle.setAttribute('stroke', 'black');
+    circle.setAttribute('fill', randomColor());
+    circle.setAttribute('stroke', randomColor());
     circle.setAttribute('cx', `${Math.random() * 200}`);
     circle.setAttribute('cy', `${Math.random() * 200}`);
     circle.setAttribute('r', `${Math.random() * 50}`);
     shape.appendChild(circle);
   } else if (shapeForm === 'rect') {
-    rect.setAttribute('fill', 'white');
-    rect.setAttribute('stroke', 'black');
+    rect.setAttribute('fill', randomColor());
+    rect.setAttribute('stroke', randomColor());
     rect.setAttribute('x', `${Math.random() * 200}`);
     rect.setAttribute('y', `${Math.random() * 200}`);
     rect.setAttribute('width', `${Math.random() * 100}`);
-    rect.setAttribute('height', `${Math.random() * 100}`);
+    rect.setAttribute('height', `${Math.random() * 80}`);
     shape.appendChild(rect);
   } else if (shapeForm === 'triangle') {
-    triangle.setAttribute('fill', 'lightblue');
-    triangle.setAttribute('stroke', 'green');
+    triangle.setAttribute('fill', randomColor());
+    triangle.setAttribute('stroke', randomColor());
     triangle.setAttribute('x1', `${Math.random() * 200}`);
     triangle.setAttribute('y1', `${Math.random() * 200}`);
     triangle.setAttribute('x2', `${Math.random() * 200}`);
