803bf2a396c8d99c1edc4c26b2e06717fb065b6c
ChrisNgy <c.nagy@students.htl-leonding.ac.at>
2025-10-09 13:02:43 +0200

    Refactor shape generation logic: remove shape selection and streamline circle and ellipse creation based on input values

--- projects/create-shapes/src/index.ts
+++ projects/create-shapes/src/index.ts
@@ -2,39 +2,39 @@ const spalten = document.getElementById('spalten') as HTMLInputElement;
 const zeilen = document.getElementById('zeilen') as HTMLInputElement;
 const generator = document.getElementById('generator') as HTMLButtonElement;
 const svg = document.getElementById('svg') as unknown as SVGSVGElement;
-const select = document.getElementById('shapeForm') as HTMLSelectElement;
 
 generator.addEventListener('click', () => {
-  const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
-  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect') as SVGRectElement;
-  const ellipse = document.createElementNS('http://www.w3.org/2000/svg', 'ellipse') as SVGEllipseElement;
-  const zeile = 50;
-  const spalte = 50;
+  const zeile = parseInt(`${zeilen.value}`);
+  const spalte = parseInt(`${spalten.value}`);
+  console.log(zeile);
+  console.log(spalten);
 
-  if (select.value === 'round') {
-    if (spalten.value === zeilen.value) {
-      circle.setAttribute('fill', 'white');
-      circle.setAttribute('stroke', 'black');
-      circle.setAttribute('cx', `${200}`);
-      circle.setAttribute('cy', `${200}`);
-      circle.setAttribute('r', `${zeile * parseInt(`${zeilen.value}`)}`);
-      svg.appendChild(circle);
-    } else {
-      ellipse.setAttribute('fill', 'white');
-      ellipse.setAttribute('stroke', 'black');
-      ellipse.setAttribute('cx', `${200}`);
-      ellipse.setAttribute('cy', `${200}`);
-      ellipse.setAttribute('rx', `${spalte * parseInt(`${zeilen.value}`)}`);
-      ellipse.setAttribute('ry', `${zeile * parseInt(`${spalten.value}`)}`);
-      svg.appendChild(ellipse);
+  if (zeilen.value === spalten.value) {
+    for (let x = 0; x < zeile; x++) {
+      for (let y = 0; y < spalte; y++) {
+        const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
+        circle.setAttribute('fill', 'white');
+        circle.setAttribute('stroke', 'black');
+        circle.setAttribute('cx', `${(400 / zeile) * x + 100}`);
+        circle.setAttribute('cy', `${(400 / spalte) * y + 100}`);
+        circle.setAttribute('r', `${400 / (spalte + zeile)}`);
+        svg.appendChild(circle);
+        console.log(circle);
+      }
     }
-  } else if (select.value === 'square') {
-    rect.setAttribute('fill', 'white');
-    rect.setAttribute('stroke', 'black');
-    rect.setAttribute('x', `${200 - zeile * parseInt(`${zeilen.value}`)}`);
-    rect.setAttribute('y', `${200 - spalte * parseInt(`${spalten.value}`)}`);
-    rect.setAttribute('whidth', `${zeile * parseInt(`${zeilen.value}`)}`);
-    rect.setAttribute('height', `${spalte * parseInt(`${spalten.value}`)}`);
-    svg.appendChild(rect);
+  } else {
+    for (let x = 0; x < zeile; x++) {
+      for (let y = 0; y < spalte; y++) {
+        const ellipse = document.createElementNS('http://www.w3.org/2000/svg', 'ellipse') as SVGEllipseElement;
+        ellipse.setAttribute('fill', 'white');
+        ellipse.setAttribute('stroke', 'black');
+        ellipse.setAttribute('cx', `${(400 / zeile) * x + 400 / zeile / 2}`);
+        ellipse.setAttribute('cy', `${(400 / spalte) * y + 400 / spalte / 2}`);
+        ellipse.setAttribute('rx', `${400 / zeile / 2}`);
+        ellipse.setAttribute('ry', `${400 / spalte / 2}`);
+        svg.appendChild(ellipse);
+        console.log(ellipse);
+      }
+    }
   }
 });
